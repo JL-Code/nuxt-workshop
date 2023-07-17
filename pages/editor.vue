0c2e690f954e508cf61823e03c65fe5d649ff896
蒋勇 <jiangy@highzap.com>
2023-07-17 17:35:53 +0800

    init project

---
+++ pages/editor.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="markdown-editor">
+    <md-editor
+      v-model="text"
+      :toolbarsExclude="toolbarsExclude"
+      noUploadImg
+      @onChange="onChange"
+      @onSave="onSave"
+      @onError="onError"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { MdEditor } from "md-editor-v3";
+import type { ToolbarNames } from "md-editor-v3";
+import "md-editor-v3/lib/style.css";
+
+const text = ref("");
+const toolbarsExclude: ToolbarNames[] = [
+  "github",
+  "mermaid",
+  "katex",
+  "htmlPreview",
+];
+const toolbars: ToolbarNames[] = ["italic", "underline", "-", "bold", "="];
+const onChange = (value: string) => {};
+const onError = (err: { name: string; message: string }) => {};
+const onSave = (value: string, html: Promise<string>) => {
+  console.log("onSave", value);
+};
+const onUploadImg = (
+  files: Array<File>,
+  callback: (urls: Array<string>) => void
+) => {
+  console.log(Array.from(files));
+};
+</script>
+
+<style lang="less"></style>
---
+++ pages/preview.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="md-preview">
+    <MdPreview :editorId="id" :modelValue="text" />
+    <ClientOnly>
+      <MdCatalog :editorId="id" :scrollElement="scrollElement" />
+    </ClientOnly>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import { MdPreview, MdCatalog } from "md-editor-v3";
+import "md-editor-v3/lib/preview.css";
+
+const id = "preview-only";
+const text = ref("# Hello Editor");
+const scrollElement = ref<HTMLElement>();
+
+onMounted(() => {
+  scrollElement.value = document.documentElement;
+});
+</script>
